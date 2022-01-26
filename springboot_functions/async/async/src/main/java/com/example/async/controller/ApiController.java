54d85cb65d2d7aeda738580b610475e7f0aa4a69
devpola <sonamoo9771@gmail.com>
2022-01-26 16:39:37 +0900

    Add async examples

---
+++ springboot_functions/async/async/src/main/java/com/example/async/controller/ApiController.java
@@ -0,0 +1,30 @@
+package com.example.async.controller;
+
+import com.example.async.service.AsyncService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @Autowired
+    private AsyncService asyncService;
+
+    public ApiController(AsyncService asyncService) {
+        this.asyncService = asyncService;
+    }
+
+    //CompletableFuture는 다른 thread에서 함수를 실행시키고 결과를 반환 받는 형태
+    @GetMapping("/hello")
+    public CompletableFuture hello(){
+        log.info("completable future init");
+        return asyncService.run();
+    }
+}
--- springboot_functions/async/async_src/AsyncApplication.java
+++ springboot_functions/async/async_src/AsyncApplication.java
@@ -1,13 +1,15 @@
-package com.example.interceptor;
+package com.example.async;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-public class InterceptorApplication {
+@EnableAsync
+public class AsyncApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InterceptorApplication.class, args);
+		SpringApplication.run(AsyncApplication.class, args);
 	}
 
 }
